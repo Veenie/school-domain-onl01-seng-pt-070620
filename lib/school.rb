20b5afbbeedd561046d06a790017a0b46790d83f
Raveen K Sharma <raveen08@gmail.com>
2020-07-22 20:56:11 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -10,8 +10,8 @@ class School
 
   def add_student(student_name, grade)
     roster[grade] = []
-    if roster[grade]
-      roster[grade] << student_name
+    if roster[grade].include?[grade]
+      roster << student_name
     end
   end
 
